7080e103a4c8411986074a40da77bf864626e5d3
LoLaparr <louis.laparre@hotmail.com>
2024-08-23 17:10:51 +0200

    cpp09: reprise doucement mais tres surement plus que 2 fnct et l'ex00 fini

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -1,33 +1,100 @@
 #pragma once
 
-#include <ctime>
 #include <map>
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
-class BitcoinExchange {
+class BitcoinExchange
+{
 private:
-	std::map<time_t, float> _data;
-	float getValueAt(const time_t &time) const;
+	std::map<std::string, float> _exchange;
 
 public:
 	BitcoinExchange();
-
-	/**
-		* @brief Load data from a file
-		*
-		* @param filename
-	*/
-	BitcoinExchange(const char *filename);
-
-	BitcoinExchange(const BitcoinExchange &other);
-	BitcoinExchange &operator=(const BitcoinExchange &other);
+	BitcoinExchange::BitcoinExchange(const BitcoinExchange &other): _exchange(other._exchange) {}
+	BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other);
 	~BitcoinExchange();
 
-		/**
-		* @brief Process an input file, based on the following format:
-		*		<date> | <price>
-		*
-		* @param filename
-		*/
-	void process(const char *filename);
+	void BitcoinExchange::loadDatabase(const std::string &filename);
+	void BitcoinExchange::processInputFile(const std::string &inputFilename);
 };
+
+void BitcoinExchange::loadDatabase(const std::string &filename) {
+
+	std::ifstream	file(filename);
+	if (!file.is_open()) {
+		std::cerr << "Error: File couldn't open the file " << filename << std::endl;
+		return 1;
+	}
+	std::string	line;
+	while(std::getline(file, line)) {
+		std::string	date;
+		float rate;
+		std::stringstream	ss(line);
+
+		if (std::getline(ss, date, ',') && ss >> rate) {
+			_exchange[date] = rate;
+		}
+		else
+			std::cerr << "Bad line in " << filename << std::endl;
+	}
+	file.close();
+
+}
+
+void BitcoinExchange::processInputFile(const std::string &inputFilename) {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other) {
+	if (this != &other) {
+		_exchange = other._exchange;
+	}
+	return *this;
+}
+
+BitcoinExchange::BitcoinExchange()
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
